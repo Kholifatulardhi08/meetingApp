999be1c57cc5ac55db0ff3c239316db474a78ec6
Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>
2022-07-20 15:00:52 +0700

    membuat halaman multi user
    Signed-off-by: Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>

--- app/Http/Controllers/Backend/UnitController.php
+++ app/Http/Controllers/Backend/UnitController.php
@@ -14,13 +14,6 @@ class UnitController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    function __construct()
-    {
-      $this->middleware('permission:user-list', ['only' => ['index','show']]);
-      $this->middleware('permission:user-create', ['only' => ['create','store']]);
-      $this->middleware('permission:user-edit', ['only' => ['edit','update']]);
-      $this->middleware('permission:user-delete', ['only' => ['destroy']]);
-    }
 
     public function index(Request $request)
     {
